3dfee568c0c7844488301d78dfe33ed69627ed21
dreyemi <dreyemi@04881de9-fdbf-516b-55c9-ec6914764f8b>
2010-08-24 14:29:07 +0000

    modified scripts.js and committed to HEAD
    git-svn-id: https://drupalcamplagos.googlecode.com/svn/trunk@71 04881de9-fdbf-516b-55c9-ec6914764f8b

--- sites/all/themes/drupalnigeria/js/scripts.js
+++ sites/all/themes/drupalnigeria/js/scripts.js
@@ -8,11 +8,6 @@ $(document).ready(function() {
     $('form#simplenews-block-form-1 label[@class=option]').replaceWith('');
     $('#block-simplenews-1 div[@class=issues-link]').replaceWith('');
     $('#block-simplenews-1 a[@class=feed-icon]').replaceWith('');
-    //$('form#simplenews-block-form-6 > input').remove();
-    //$('div.form-radios').remove('#edit-action-subscribe-wrapper');
-    //rmbtnlbl.attr('value', '', '');
-    //alert(simplenewsformhide);
-    //simplenewsformhide.replaceWith('');
 });
 (function($){
 
